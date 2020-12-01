390ebdec160eb84de9f68e9bcc48b76e5ab89075
Yuka Sadaoka <yukasadaoka@outlook.com>
2020-12-01 17:14:23 -0500

    Added authentication to the profile API

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,7 +8,9 @@ router = DefaultRouter()
 router.register('message-viewset', views.MessageViewSet, base_name='message-viewset')
 router.register('profile', views.UserProfileViewSet) ## don't need to give base_name because UserProfileViewSet has queryset
 
+
 urlpatterns = [
     path('random-view', views.RandomApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]

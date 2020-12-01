8abcfcb6015a67b27a98a6295740ea937041d916
Yuka Sadaoka <yukasadaoka@outlook.com>
2020-12-01 17:01:13 -0500

    Added a serching function to Profile API

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 from profiles import serializers
 from profiles import models
@@ -100,3 +102,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     # To make authentication_classes as a tuple by adding , in the end
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email', )

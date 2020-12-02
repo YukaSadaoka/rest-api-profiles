4e0816ff98e4ebfba21d07d29b3ad04de71010b1
Yuka Sadaoka <yukasadaoka@outlook.com>
2020-12-01 20:21:45 -0500

    Added user profile feed API

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -3,9 +3,18 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
-
     def has_object_permission(self, request, iew, obj):
         """Check user if they have permissions to edit the target profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
         return  obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+    def has_object_permission(self, request, iew, obj):
+        """ Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- profiles/urls.py
+++ profiles/urls.py
@@ -5,9 +5,9 @@ from rest_framework.routers import DefaultRouter
 from profiles import views
 
 router = DefaultRouter()
-router.register('message-viewset', views.MessageViewSet, base_name='message-viewset')
+router.register('message', views.MessageViewSet, base_name='message-viewset')
 router.register('profile', views.UserProfileViewSet) ## don't need to give base_name because UserProfileViewSet has queryset
-
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('random-view', views.RandomApiView.as_view()),
